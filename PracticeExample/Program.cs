234f6bfb9872bd48fba6e1afa4d004933270b172
jeetyadu <visyadav9005@gmail.com>
2023-09-13 19:17:10 +0530

    Add project files.

---
+++ PracticeExample/Program.cs
@@ -0,0 +1,19 @@
+﻿public delegate int SampleDelegate();
+public class Program
+{
+    public static int Method1()
+    {
+        return 1;
+    }
+    public static int Method2()
+    {
+        return 2;
+    }
+    static void Main(string[] args)
+    {
+        SampleDelegate sampleDelegate = new SampleDelegate(Program.Method1);
+        sampleDelegate += Program.Method2;
+        int returnDelegateType = sampleDelegate();
+        Console.WriteLine(returnDelegateType);
+    }
+}
